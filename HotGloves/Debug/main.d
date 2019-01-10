68e68dc2b2fe9c5e4827cd727d9b9f389976e69e
Alex Schumann <noch11minuten@gmx.de>
2019-01-10 11:27:21 +0100

    debug send battery, temp, level via uart

--- HotGloves/Debug/main.d
+++ HotGloves/Debug/main.d
@@ -18,7 +18,10 @@ main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h \
  ../lcdlib/lcd.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdbool.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
 
@@ -59,3 +62,9 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdbool.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
--- HotGloves/main.c
+++ HotGloves/main.c
@@ -3,7 +3,7 @@
  *
  * Created: 14.11.2018
  * Author : Alex Schumann
- * Version : 0.3
+ * Version : 0.5
  */ 
 
 #define F_CPU	3686400UL	// also manually defined in delay.h
@@ -16,35 +16,42 @@
 #include <lcd.h>
 #include <stdbool.h>
 #include <string.h>
-//#include <avr/pgmspace.h>
-//#include <util/delay.h>
-//#include <time.h>
-
+#include <util/delay.h>
 
 #define LCD_PORT	PORTC
 #define NAME_OF_BM	"HotGlove"
 
-#define LEVEL0IN	0b00110000
-#define LEVEL1IN	0b00110001
-#define LEVEL2IN	0b00110010
-#define LEVEL3IN	0b00110011
-#define LEVEL4IN	0b00110100
-#define LEVEL5IN	0b00110101
+#define LEVEL0IN	0b00110000	//ASCII 0
+#define LEVEL1IN	0b00110001	//ASCII 1
+#define LEVEL2IN	0b00110010	//ASCII 2
+#define LEVEL3IN	0b00110011	//ASCII 3
+#define LEVEL4IN	0b00110100	//ASCII 4
+#define LEVEL5IN	0b00110101	//ASCII 5
 
-#define LEVEL0OUT	0b11111111
-#define LEVEL1OUT	0b11111110
-#define LEVEL2OUT	0b11111100
-#define LEVEL3OUT	0b11111000
-#define LEVEL4OUT	0b11110000
-#define LEVEL5OUT	0b11100000
+#define MAXTEMP1 25
+#define MAXTEMP2 35
+#define MAXTEMP3 40
+#define MAXTEMP4 45
+#define MAXTEMP5 55
 
 double dutyCycle = 0;
+char* level;
 int lvl;
+int pwm;
 
-void init_led() {
-	DDRC = 0xFF;    // LEDs - output
-	PORTC = 0b11111111; // LEDs - off
-}
+//Function definitions
+void init_lcd();
+void init_uart();
+void init_bluetooth();
+void init_ADC();
+void init_PWM();
+bool isReceivedValueValid(unsigned int receivedValue);
+void outputAdcAndVoltAndLevelAndPwmOnLcd();
+void setLevel(char value);
+void serialSend(char* sendString);
+void debugInput();
+int main(void);
+
 
 void init_lcd() {
 	lcd_init(LCD_DISP_ON);
@@ -55,18 +62,24 @@ void init_lcd() {
 }
 
 void init_uart() {
-	uart_init( UART_BAUD_SELECT(UART_BAUD_RATE,F_CPU) );
+	uart_init( UART_BAUD_SELECT(UART_BAUD_RATE,F_CPU) );	
 }
 
 void init_bluetooth(){
+	_delay_ms(500);
 	uart_puts_P("$$$\r");
 	uart_puts_P("+\r");
+	_delay_ms(500);
+	
 	char renameString[20];
 	strcpy(renameString, "sn,");
 	strcat(renameString, NAME_OF_BM);
 	strcat(renameString, "\r");
 	uart_puts(renameString);
-	uart_puts_P("R,1\r");
+	_delay_ms(500);
+	
+	uart_puts_P("\rR,1\r");
+	_delay_ms(500);
 }
 
 void init_ADC() {
@@ -81,147 +94,204 @@ void init_PWM() {
 	OCR0A = (dutyCycle/100) * 255.0;
 }
 
-// I don't think this works at all - however it seems to be needless anyway
-// I think it's because of the '&' which wouldn't work used on a param instead of the original received value
 bool isReceivedValueValid(unsigned int receivedValue) {
-	char errorMsg[20];
-	bool error = false;
-	if ( receivedValue & UART_FRAME_ERROR ) {
-		strcpy(errorMsg, "UART Frame Error");
-		error = true;
-    } 
-	if ( receivedValue & UART_OVERRUN_ERROR ) {
-        strcpy(errorMsg, "UART Overrun Error");
-        error = true;
-	} 
-	if ( receivedValue & UART_BUFFER_OVERFLOW ) {
-		strcpy(errorMsg, "Buffer overflow error");
-		error = true;
-    }
-			
-   	if (error) {
-   		lcd_puts(errorMsg);
-   		uart_puts(errorMsg);
-   		return false;
-   	} 
+	if ( receivedValue & UART_FRAME_ERROR || receivedValue & UART_OVERRUN_ERROR || receivedValue & UART_BUFFER_OVERFLOW ) {
+		lcd_puts("UARTERR");
+		return false;
+	}
 	return true;
 }
 
-void outputADC() {	
+void outputAdcAndVoltAndLevelAndPwmOnLcd() {	
+	lcd_clrscr();
+	
 	lcd_gotoxy(0,0);
 	lcd_puts("ADC ");
-	char value[16];
-	itoa( ADC , value, 10);
-	lcd_puts(value);
+	char adcChar[16];
+	itoa( ADC , adcChar, 10);
+	lcd_puts(adcChar);
+	
+	lcd_gotoxy(8,0);
+	float volt = ((((float)ADC)*4.0)/1024)*1000;
+	lcd_puts("mV ");
+	char voltChar[16];
+	itoa( volt , voltChar, 10);
+	lcd_puts(voltChar);
+	
+	lcd_gotoxy(0,1);
+	lcd_puts("Level ");
+	lcd_puts(level);
+	
+	lcd_gotoxy(8,1);
+	lcd_puts("PWM ");
+	char pwmChar[16];
+	itoa( pwm , pwmChar, 10);
+	lcd_puts(pwmChar);
 }
 
-void setLevel(char value) {
- 	lcd_gotoxy(0,1);
- 	lcd_puts("Level ");
- 	lcd_gotoxy(6,1);
-	char* level;
-	
+void setLevel(char value) {	
 	switch (value) {
 		case LEVEL0IN:
 			level = "0";
 			lvl = 0;
+			serialSend("L0L");
 			break;
 		case LEVEL1IN:
 			level = "1";
 			lvl = 1;
+			serialSend("L1L");
 			break;
 		case LEVEL2IN:
 			level = "2";
 			lvl = 2;
+			serialSend("L2L");
 			break;
 		case LEVEL3IN:
 			level = "3";
 			lvl = 3;
+			serialSend("L3L");
 			break;
 		case LEVEL4IN:
 			level = "4";
 			lvl = 4;
+			serialSend("L4L");
 			break;
 		case LEVEL5IN:
 			level = "5";
 			lvl = 5;
+			serialSend("L5L");
 			break;
 		default:
 			return;
 	}
-	lcd_puts(level);
-	uart_puts(level);
+}
+
+void serialSend(char* sendString){
+	for (int i = 0; i < strlen(sendString); i++){
+		while (( UCSR0A & (1<<UDRE0))  == 0){};
+		UDR0 = sendString[i];
+	}
 }
 
 void debugInput(){
 	if(!(PINA & _BV(PINA2))) {
 		setLevel(0b00110000);
+		_delay_ms(500);
+		serialSend("T12T");
 	}
 	if(!(PINA & _BV(PINA3))) {
 		setLevel(0b00110001);
+		_delay_ms(500);
+		serialSend("T24T");
 	}
 	if(!(PINA & _BV(PINA4))) {
 		setLevel(0b00110010);
+		_delay_ms(500);
+		serialSend("T35T");
 	}
 	if(!(PINA & _BV(PINA5))) {
 		setLevel(0b00110011);
+		_delay_ms(500);
+		serialSend("B99B");
 	}
 	if(!(PINA & _BV(PINA6))) {
 		setLevel(0b00110100);
+		_delay_ms(500);
+		serialSend("B78B");
 	}
 	if(!(PINA & _BV(PINA7))) {
 		setLevel(0b00110101);
+		_delay_ms(500);
+		serialSend("B32B");
 	}
 }
 
-int main(void)
-{
-	DDRA = 0x00; // quickinput for debugging
-	//init_led();
+int main(void) {
+	DDRA = 0x00; // cheat
+	
 	init_lcd();
 	init_uart();
 	init_bluetooth();
 	init_PWM();
 	init_ADC();
 		
-	TCCR0B =  (1<<CS00); //| (1<<CS01) ;//(1<<CS02); //starts timer?
-	sei(); // set external interrupts
-		
-	OCR0A = 128;
+	TCCR0B =  (1<<CS00); //| (1<<CS01) ;//(1<<CS02); 
+	sei(); // set external interrupts - has to be done last
 	
-	unsigned int receivedValue;
-	unsigned char value = 0b11111111;
+	unsigned int valueReceivedViaBT;
+	unsigned char cachedValue = 0b11111111;
 	
-    while (1) 
-    {
-        receivedValue = uart_getc();
+    while (1) {
+	    debugInput();	// cheat
 		
-		// '&' in C means 'address-of' - but what the hell does it mean here?
-		if ( receivedValue & UART_NO_DATA) {
+        valueReceivedViaBT = uart_getc();
+		if ( valueReceivedViaBT & UART_NO_DATA) {
 			// no data available from UART
 		} else {
-			isReceivedValueValid(receivedValue);
+			isReceivedValueValid(valueReceivedViaBT); // no need to do that
 			
-			// is the new input a new value?
-			if (value != (unsigned char) receivedValue) {
-				value = (unsigned char) receivedValue;
-				setLevel(value);
+			if (cachedValue != (unsigned char) valueReceivedViaBT) {
+				cachedValue = (unsigned char) valueReceivedViaBT;
+				setLevel(cachedValue);
 			}
 		}
 		
-		debugInput();
 		
-		OCR0A = lvl * 51;	
+		float volt = (((float)ADC)*4.0)/1024;
+		float celcius = 701.41*volt*volt*volt - 2838.1*volt*volt + 3839*volt - 1700.1;
+		int maxTempNotReached = 1;
 		
-		outputADC();
+		switch (lvl) {
+			case 1:
+				if ( celcius > MAXTEMP1 ) {
+					maxTempNotReached = 0;
+				} else {
+					maxTempNotReached = 1;
+				}
+				break;
+			case 2:
+				if ( celcius > MAXTEMP2 ) {
+					maxTempNotReached = 0;
+					} else {
+					maxTempNotReached = 1;
+				}
+				break;
+			case 3:
+				if ( celcius > MAXTEMP3 ) {
+					maxTempNotReached = 0;
+					} else {
+					maxTempNotReached = 1;
+				}
+				break;
+			case 4:
+				if ( celcius > MAXTEMP4 ) {
+					maxTempNotReached = 0;
+					} else {
+					maxTempNotReached = 1;
+				}
+				break;
+			case 5:
+				if ( celcius > MAXTEMP5 ) {
+					maxTempNotReached = 0;
+					} else {
+					maxTempNotReached = 1;
+				}
+				break;
+		}
+		
+		pwm = lvl * 51 * maxTempNotReached;
+		
+		OCR0A = pwm;	
+		outputAdcAndVoltAndLevelAndPwmOnLcd();
     }
 }
 
+//Interrupts
 ISR (TIMER0_OVF_vect) {
- 		
+
 }
 
 ISR (ADC_vect) {
- 
+
 }
 
